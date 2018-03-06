3877f3859143c5ab2128a3ce2dc20c4a3a36203b
Jake LeBoeuf <dev@jakeleboeuf.com>
2018-03-06 11:07:00 -0500

    works

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var resolve = require('resolve');
 var Funnel = require('broccoli-funnel');
 var MergeTrees = require('broccoli-merge-trees');
 var map = require('broccoli-stew').map;
-var remodalPath = path.dirname(resolve.sync('remodal'));
 
 module.exports = {
   name: 'ember-remodal',
 
   treeForVendor(defaultTree) {
+    var remodalPath = path.dirname(resolve.sync('remodal'));
     var remodalTree = new Funnel(remodalPath, {
       files: ['remodal.min.js'],
       destDir: '/remodal/dist'
@@ -25,6 +25,7 @@ module.exports = {
   },
 
   treeForStyles: function(defaultTree) {
+    var remodalPath = path.dirname(resolve.sync('remodal'));
     var remodalTree = new Funnel(remodalPath, {
       files: ['remodal.css', 'remodal-default-theme.css'],
       destDir: '/remodal/dist'
@@ -44,10 +45,7 @@ module.exports = {
     // In ember-cli < 2.7, this.import is not available, so fall back to use app.import
     var importShim = typeof this.import !== 'undefined' ? this : app;
 
-    if (!process.env.EMBER_CLI_FASTBOOT) {
-      importShim.import('vendor/remodal/dist/remodal.min.js');
-    }
-
+    importShim.import('vendor/remodal/dist/remodal.min.js');
     importShim.import('vendor/remodal/dist/remodal.css');
     importShim.import('vendor/remodal/dist/remodal-default-theme.css');
     importShim.import('vendor/style.css');
