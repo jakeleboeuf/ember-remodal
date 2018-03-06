de69af2dfd9eb733dc3fa6cb9d145ced474f1010
Jake LeBoeuf <dev@jakeleboeuf.com>
2018-03-06 13:10:21 -0500

    default include

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ module.exports = {
   treeForVendor(defaultTree) {
     var remodalPath = path.dirname(resolve.sync('remodal'));
     var remodalTree = new Funnel(remodalPath, {
-      files: ['remodal.min.js'],
-      destDir: '/remodal/dist'
+      files: ['remodal.min.js']
     });
 
     remodalTree = map(
@@ -29,8 +28,7 @@ module.exports = {
   treeForStyles: function(defaultTree) {
     var remodalPath = path.dirname(resolve.sync('remodal'));
     var remodalTree = new Funnel(remodalPath, {
-      files: ['remodal.css', 'remodal-default-theme.css'],
-      destDir: '/remodal/dist'
+      files: ['remodal.css', 'remodal-default-theme.css']
     });
 
     remodalTree = map(
@@ -44,14 +42,9 @@ module.exports = {
   },
 
   included: function(app) {
-    this._super.included(app);
-
-    // In ember-cli < 2.7, this.import is not available, so fall back to use app.import
-    var importShim = typeof this.import !== 'undefined' ? this : app;
-
-    importShim.import('vendor/remodal/dist/remodal.min.js');
-    importShim.import('vendor/remodal/dist/remodal.css');
-    importShim.import('vendor/remodal/dist/remodal-default-theme.css');
-    importShim.import('vendor/style.css');
+    this.import('vendor/remodal.min.js');
+    this.import('vendor/remodal.css');
+    this.import('vendor/remodal-default-theme.css');
+    this.import('vendor/style.css');
   }
 };
